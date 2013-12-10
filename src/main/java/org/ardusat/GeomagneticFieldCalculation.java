e2bd392ee0591840c71a4fa3d5622ac98425f127
Pablo Pita <pablo.pita@gmail.com>
2013-12-10 15:50:58 +0100

    Computation of alt,lon,lat with Ellipsoid

--- src/main/java/org/ardusat/GeomagneticFieldCalculation.java
+++ src/main/java/org/ardusat/GeomagneticFieldCalculation.java
@@ -7,8 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.util.FastMath;
 import org.orekit.attitudes.InertialProvider;
+import org.orekit.bodies.GeodeticPoint;
+import org.orekit.bodies.OneAxisEllipsoid;
+import org.orekit.data.DataProvider;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.data.DirectoryCrawler;
 import org.orekit.errors.OrekitException;
+import org.orekit.frames.FramesFactory;
 import org.orekit.models.earth.GeoMagneticElements;
 import org.orekit.models.earth.GeoMagneticField;
 import org.orekit.models.earth.GeoMagneticFieldFactory;
@@ -20,11 +27,10 @@ import org.orekit.propagation.sampling.OrekitFixedStepHandler;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.DateComponents;
 import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.Constants;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-
-import fr.cs.examples.Autoconfiguration;
 
 /**
  * <p>This class calculates the geomagnetic field components of an earth orbiting 
@@ -39,6 +45,7 @@ import fr.cs.examples.Autoconfiguration;
  * <a href="https://www.orekit.org/forge/projects/orekit/wiki/GeomagneticFieldTutorial">GeomagneticFieldTutorial</a>
  * for more information. 
  * 
+ * 
  * @author pablo.pita@gmail.com
  * @license Apache v.2
  */
@@ -48,26 +55,34 @@ public class GeomagneticFieldCalculation implements Runnable
 
 	public static void main( String[] args ) throws OrekitException
 	{
-		Autoconfiguration.configureOrekit();
 		new GeomagneticFieldCalculation().run();
 	}
 
 	// Load our own config values from the default resource location, application.conf	
-	static Config conf = ConfigFactory.load();
 	final double duration;
 	final int timestep; 
-	private String output = "geomagnetic_field.dat";
+	final String output;
+	final String outDir;
 	final GeoMagneticField model;
 	final TLE tle;
 	final double year;
 
 	GeomagneticFieldCalculation() throws OrekitException {
-		String tle1 = conf.getString("tle.line.1");
-		String tle2 = conf.getString("tle.line.2");
-		tle = new TLE(tle1, tle2);
-		duration = Double.parseDouble(conf.getString("duration"));
-		timestep = Integer.parseInt(conf.getString("timestep"));
-		output = conf.getString("output");
+		Config conf = ConfigFactory.load();
+		outDir = conf.hasPath("output.dir") ? conf.getString("output.dir") : System.getProperty("user.home");
+		output = conf.hasPath("output.file") ? conf.getString("output.file") : "ardusat_geomagnetic_field.dat";
+		String orekitData = conf.hasPath("orekit") ?  conf.getString("orekit") : System.getProperty("user.home") + "/.orekit-data";
+		DataProvider provider = new DirectoryCrawler(new File(orekitData));
+		DataProvidersManager.getInstance().addProvider(provider);
+		if (conf.hasPath("tle.line.1") && conf.hasPath("tle.line.2")) {
+			tle = new TLE(conf.getString("tle.line.1"), conf.getString("tle.line.2"));
+		} else {
+			// 39412U object related to the ISS for the date 2013-12-09
+			tle = new TLE("1 39412U 98067DA  13343.12890038  .00068429  00000-0  10624-2 0   630",
+					"2 39412  51.6509 321.3059 0005304  96.9471 359.5554 15.53317866  3035");
+		}
+		duration = conf.hasPath("duration") ? conf.getDouble("duration") : 6000;
+		timestep = conf.hasPath("timestep") ? conf.getInt("timestep") : 20;
 		DateComponents dt = tle.getDate().getComponents(TimeScalesFactory.getUTC()).getDate();
 		year = GeoMagneticField.getDecimalYear(dt.getDay(), dt.getMonth(), dt.getYear());
 		model = GeoMagneticFieldFactory.getWMM(year);
@@ -79,55 +94,81 @@ public class GeomagneticFieldCalculation implements Runnable
 
 	@Override
 	public void run() {
-			StatesHandler stepHandler = new StatesHandler();
-			try {
-				List<SpacecraftState> states = calculateStates(tle, duration, timestep, stepHandler);
-				save(states);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (OrekitException e) {
-				e.printStackTrace();
-			}
+		StatesHandler stepHandler = new StatesHandler();
+		try {
+			List<SpacecraftState> states = calculateStates(tle, duration, timestep, stepHandler);
+			save(states);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (OrekitException e) {
+			e.printStackTrace();
+		}
 	}
 
-
-	private void save(List<SpacecraftState> states) throws IOException {
-		// calculate the geomagnetic field : lat, lon, alt
-		// Date   Alt  Lat  Lon        X        Y         Z        H        F        I       D
-		//         km  deg  deg       nT       nT        nT       nT       nT      deg     deg
-        // 2010.0 100   80,   0,  6332.2,  -729.1,  52194.9,  6374.0, 52582.6,  83.04, -6.57},
-        // 2012.5 100,   0, 120, 37448.1,   559.7, -11044.2, 37452.2, 39046.7, -16.43,  0.86},
-		// Let's write the results in a file in order to draw some plots.
-		final File file = new File(System.getProperty("user.home"), output);
+	/**
+	 * Computes altitude, latitude and longitude
+	 *  
+	 * The TLE propagator does not propagate orbits in the EME2000 frame but rather in the TEME frame, 
+	 * which is a strange frame used only by TLE. Due to precession, the TEME frame is not fixed 
+	 * with respect to EME2000.
+	 * 
+	 * The OneAxisEllipsoid object should not be set up with an inertially oriented frame like the EME2000, 
+	 * but should should be set up with a frame that represents Earth rotation 
+	 * (i.e. which is fixed with respect to Earth), like ITRF. 
+	 * 
+	 * We give a position in ITRF, then we use ITRF as the second argument. 
+	 * You cannot mix. The reason for that is that if you give a position in first argument, 
+	 * you specify in the second argument in which frame you did provide the first argument, 
+	 * then the object which remembers the rotating frame in which it was built will do the transform 
+	 * if it need to. 
+	 * So if you decided to already provides the position in ITRF *and* passed ITRF as the second argument, 
+	 * then the object will notice it is already the same frame it was built with and it will not transform 
+	 * the cartesian position before computing the angle.
+	 *  
+	 * If on the other hand you provide a position in EME2000 *and* pass EME2000 as the second argument, 
+	 * then the object will notice EME2000 is not the same as the ITRF frame it was built with, 
+	 * so it will first perform the transformation to convert the EME2000 position into ITRF 
+	 * and then compute the angles. 
+	 */
+	private void save(List<SpacecraftState> states) throws IOException, OrekitException {
+		final File file = new File(outDir, output);
 		final PrintStream out = new PrintStream(file);
-		//          0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-		out.format("Date   Alt   Lat   Lon         X         Y         Z         H         F        I       D\n");
-		out.format("        km   deg   deg        nT        nT        nT        nT        nT      deg     deg\n");
-//		System.out.format("Date   Alt   Lat   Lon         X         Y         Z         H         F        I       D\n");
-//		System.out.format("        km   deg   deg        nT        nT        nT        nT        nT      deg     deg\n");
-
+		printOutputHeader(out);
+		// printOutputHeader(System.out);
+		
 		for (SpacecraftState state : states) {
 			Orbit o = state.getOrbit();
-			double alt = (o.getA() - 6378135)/1000;
-			
-			// How do we calculate the latitude and longitude from the satellite orbit?
-			// which frames are involved in Orekit for that?
-			Vector3D p = o.getPVCoordinates().getPosition();
-			double lon = Math.atan (p.getY() / p.getX()) * 180 / Math.PI;
-			double lat = Math.asin (p.getZ() / o.getA()) * 180 / Math.PI; 
-
+			Vector3D p = o.getPVCoordinates(FramesFactory.getITRF2008()).getPosition();			
+			OneAxisEllipsoid oae = new OneAxisEllipsoid(
+					Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+					Constants.WGS84_EARTH_FLATTENING,
+					FramesFactory.getITRF2008());
+			GeodeticPoint gp = oae.transform(p, FramesFactory.getITRF2008(), o.getDate());
+			double alt = gp.getAltitude() / 1000;
+			double lat = FastMath.toDegrees(gp.getLatitude());
+			double lon = FastMath.toDegrees(gp.getLongitude());
 			GeoMagneticElements elem = model.calculateField(lon, lat, alt);
 			final Vector3D b = elem.getFieldVector();
-			System.out.format("%6.1f %3d %5.1f %5.1f ", year, (int) alt, lat, lon);
-			System.out.format("%9.2f %9.2f %9.2f %9.2f %9.2f \n",
-			b.getX(), b.getY(), b.getY(),elem.getHorizontalIntensity(),elem.getTotalIntensity());
-			out.format("%6.1f %3d %5.1f %5.1f ", year,(int)  alt, lat, lon);
-			out.format("%9.2f %9.2f %9.2f %9.2f %9.2f ", 
-					b.getX(), b.getY(), b.getY(),elem.getHorizontalIntensity(),elem.getTotalIntensity());
-			out.format("%8.1f %7.1f\n", elem.getInclination(),elem.getDeclination());
+			// printOutput(System.out, alt, lat, lon, b, elem);
+			printOutput(out, alt, lat, lon, b, elem);
 		}
 		out.close();
 		System.out.println("Check output file: " + output);	
+	}
+
+	private void printOutputHeader(PrintStream stream) {
+		stream.format("Date   Alt   Lat   Lon         X         Y         Z         H         F        I       D\n");
+		stream.format("        km   deg   deg        nT        nT        nT        nT        nT      deg     deg\n");	
+	}
+	
+	private void printOutput(PrintStream stream, double alt, double lat, double lon, Vector3D b, GeoMagneticElements elem) {
+		String fmt1 = "%6.1f %3d %5.1f %5.1f ";
+		String fmt2 = "%9.2f %9.2f %9.2f %9.2f %9.2f ";
+		String fmt3 = "%8.1f %7.1f";
+		stream.format(fmt1, year,(int)  alt, lat, lon);
+		stream.format(fmt2, 
+				b.getX(), b.getY(), b.getY(),elem.getHorizontalIntensity(),elem.getTotalIntensity());
+		stream.format(fmt3 + "\n", elem.getInclination(),elem.getDeclination());		
 	}
 
 	public static List<SpacecraftState> calculateStates(TLE tle, double duration, double timestep, StatesHandler handler) 
